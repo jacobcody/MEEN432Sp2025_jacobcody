fd03f8744ed185762cb134c671823f0033c14a84
jacobcody <jacob083210@tamu.edu>
2025-02-24 20:29:24 -0600

    Update Proj2_Track.m
    Update of Track file to include inner, outer border , and centerline

--- Project 2/Week 3/Proj2_Track.m
+++ Project 2/Week 3/Proj2_Track.m
@@ -3,14 +3,14 @@ rectY = 0;
 rectWidth  = 1300;  
 rectHeight = 400;   
 
-inner = rectangle('Position', [rectX, rectY, rectWidth, rectHeight], ...
-          'Curvature', 1, 'EdgeColor', 'k', 'LineWidth', 5);
+inner = rectangle('Position', [-192.5, 7.5, 1285, 385], ...
+          'Curvature', 1, 'EdgeColor', 'k', 'LineWidth', 2.5);
 
 track = rectangle('Position', [rectX, rectY, rectWidth, rectHeight], ...
-          'Curvature', 1, 'EdgeColor', 'k', 'LineWidth', 10);
+          'Curvature', 1, 'EdgeColor', 'r', 'LineWidth', 1.5, 'LineStyle', '--');
 
-outer = rectangle('Position', [rectX, rectY, rectWidth, rectHeight], ...
-          'Curvature', 1, 'EdgeColor', 'k', 'LineWidth', 15);
+outer = rectangle('Position', [-207.5, -7.5, 1315, 415], ...
+          'Curvature', 1, 'EdgeColor', 'k', 'LineWidth', 2.5);
 
 axis([-250, 1150, -250, 1150]);
 title('Race Track Sim');
